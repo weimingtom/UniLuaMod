aab17e1cf044925921e4f58473376f5dac2f42b3
weimingtom <weimingtom@qq.com>
2017-04-07 13:49:53 +0800

    log

--- uniluamod/UniLua/ULDebug.cs
+++ uniluamod/UniLua/ULDebug.cs
@@ -1,4 +1,6 @@
-﻿namespace UniLua.Tools
+﻿using System;
+
+namespace UniLua.Tools
 {
 	// thanks to dharco
 	// refer to https://github.com/dharco/UniLua/commit/2854ddf2500ab2f943f01a6d3c9af767c092ce75
@@ -8,14 +10,26 @@
 		public static System.Action<object> LogError = NoAction;
 
 		private static void NoAction(object msg) { }
-
+		private static void LogConsoleAction(object msg)
+		{
+			Console.WriteLine(msg.ToString());
+		}
+		
 		static ULDebug()
 		{
 
 			//Log = Logger.Log;
 			//LogError = Logger.LogError;
-            Log = NoAction;
-            LogError = NoAction;
+			if (false)
+			{
+				Log = NoAction;
+            	LogError = NoAction;
+			}
+			else
+			{
+            	Log = LogConsoleAction;
+            	LogError = LogConsoleAction;
+			}
         }
 	}
 }
